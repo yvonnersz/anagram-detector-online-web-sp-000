2e7c2ee3a4003d20ddfd593f8ffecb0c769f236d
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-08 00:32:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  @@word
+
+  def initialize(word)
+    @word = word
+    @word.each do |letters|
+      letters.sort
+    end
+    
+  end
+
+  def match(argument)
+    argument.each do |letters|
+      alphabetical_sort = letters.sort
+      
+  end
+end
