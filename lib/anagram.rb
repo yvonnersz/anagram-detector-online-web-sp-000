d808eb627c0a38ebc1321003c2b533e59de165f0
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-08 00:38:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,9 +2,17 @@
 
 class Anagram
   attr_accessor :word
+  @@sorted_word_letters = []
 
   def initialize(word)
     @word = word
+    @word.each do |letters|
+      array = letters.sort
+      @@sorted_word_letters << array
   end
 
+  def match(array)
+    array.each do |anagrams|
+      anagrams.each do |letters|
+        sorted_letters = letters.sort 
 end
